418c6c618a081ea79171b4dd1760fbd6ca5a251c
ankit <ankitrai@cybage.com>
2022-11-21 14:21:42 +0530

    product crud

---
+++ src/main/java/com/example/demo/controller/RegistrationController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.modal.User;
+import com.example.demo.service.RegistrationService;
+
+@RestController
+@CrossOrigin
+public class RegistrationController {
+	@Autowired
+	private RegistrationService service;
+	
+	@PostMapping("/registeruser")
+	public ResponseEntity<?> registerUser(@RequestBody User user) throws Exception {
+		String tempEmailId = user.getEmailId(); 
+		
+		if(tempEmailId != null && !"".equals(tempEmailId)) {
+			User userobj = service.fetchUserByEmailId(tempEmailId);
+			if(userobj != null) {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("User with "+tempEmailId+" already Exists");
+			}
+		}
+			
+		User userObj = null;
+		userObj = service.saveUser(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(userObj);
+		
+	}
+	@PostMapping("/login")
+	public ResponseEntity<?> loginUser(@RequestBody User user) throws Exception {
+		String tempEmailId = user.getEmailId();
+		String tempPass = user.getPassword();
+		User userObj = null;
+		if(tempEmailId != null && tempPass != null) {
+			userObj = service.fetchUserByEmailIdAndPassword(tempEmailId, tempPass);
+			
+		}
+		if(userObj == null) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Bad Credentials");
+		}
+		return ResponseEntity.status(HttpStatus.CREATED).body(userObj);
+	}
+
+}
---
+++ src/main/java/com/example/demo/modal/User.java
@@ -0,0 +1,58 @@
+package com.example.demo.modal;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private int id;
+	private String name;
+	private String emailId;
+	private String password;
+	
+	public User() {
+		
+	}
+	
+	public User(int id, String name, String emailId, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.emailId = emailId;
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+		
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
